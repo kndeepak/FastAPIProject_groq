3b46c08c87c35ed1ea6f559b6cc5a6894d430236
Deepak KN <55972417+kndeepak@users.noreply.github.com>
2025-02-24 16:23:14 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://your-frontend.vercel.app"],
+    allow_origins=["https://chatbot-react-teal.vercel.app"],
     # allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
